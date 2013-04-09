199d05279e07869f910c730ae2c50690ea3710d1
Andrew Hammond <andrew.george.hammond@gmail.com>
2013-04-09 10:25:46 -0700

    fix datacenter grain

--- _grains/datacenter.py
+++ _grains/datacenter.py
@@ -1,7 +1,9 @@
+from salt.utils.socket_util import ip4_addrs
+
 def datacenter():
     grains = {}
 
-    local_ip = __salt__['network.ip_addrs']()
+    local_ip = ip4_addrs()
 
     if '192.168.' in local_ip:
         grains['datacenter'] = 'incero'
