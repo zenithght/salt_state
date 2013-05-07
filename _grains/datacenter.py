1ada6cd1760198335e666f3bcf3a4a55e9b52beb
Andrew Hammond <andrew.george.hammond@gmail.com>
2013-05-06 17:42:43 -0700

    fix datacenter grain

--- _grains/datacenter.py
+++ _grains/datacenter.py
@@ -1,12 +1,19 @@
 from salt.utils.socket_util import ip4_addrs
+try:
+        from ipaddress import IPv4Address
+except ImportError:
+        from salt.util.socket_util import IPv4Address
 
 def datacenter():
     grains = {}
 
-    local_ip = ip4_addrs()
+    start = IPv4Address(u'192.168.0.0')
+    end = IPv4Address(u'192.168.255.255')
 
-    if '192.168.' in local_ip:
-        grains['datacenter'] = 'incero'
+    for ip in [IPv4Address(unicode(x)) for x in ip4_addrs()]:
+        if start <= ip <= end:
+            grains['datacenter'] = 'incero'
+            break
     else:
         grains['datacenter'] = 'rackspace'
 
